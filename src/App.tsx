f09c1c8bd7635b4c7b9e96c2106e2199e4cbb299
Mustafa Garip <51916290+sushimg@users.noreply.github.com>
2024-08-27 00:40:49 +0300

    Initial Commit

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+
+const App: React.FC = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Welcome to My React TypeScript App</h1>
+        <p>
+          This is a simple starter template for your React TypeScript project.
+        </p>
+        <button className="primary-button">Click Me</button>
+      </header>
+    </div>
+  );
+}
+
+export default App;
